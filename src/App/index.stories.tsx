9fa56c154365fdc632b8c7ddb257a6d842c82ca5
kotarella1110 <kotarella1110@gmail.com>
2022-12-20 10:03:50 +0900

    chore: add storybook

---
+++ src/App/index.stories.tsx
@@ -0,0 +1,8 @@
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { App } from ".";
+
+export default {
+  component: App,
+} as ComponentMeta<typeof App>;
+
+export const Default: ComponentStoryObj<typeof App> = {};
